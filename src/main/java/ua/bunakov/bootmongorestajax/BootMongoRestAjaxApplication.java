b5c312df30d4941902e15020c47db5e9decaa9c9
vsa2017 <boonakow@mail.ru>
2018-08-10 18:52:30 +0300

    Initial commit

---
+++ src/main/java/ua/bunakov/bootmongorestajax/BootMongoRestAjaxApplication.java
@@ -0,0 +1,12 @@
+package ua.bunakov.bootmongorestajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMongoRestAjaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMongoRestAjaxApplication.class, args);
+    }
+}
---
+++ src/test/java/ua/bunakov/bootmongorestajax/BootMongoRestAjaxApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.bunakov.bootmongorestajax;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootMongoRestAjaxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
